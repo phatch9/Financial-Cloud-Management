07cdf004ffde5d1cec7d1eccc64bbb6697dc05ad
Phat <phat.chauu26@gmail.com>
2025-10-02 17:19:09 -0700

    failed server maven config - fixed

--- server/src/main/java/com/cloudmanagement/server/ServerApplication.java
+++ server/src/main/java/com/cloudmanagement/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.cloudmanagment.server;
+package com.cloudmanagement.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/com/cloudmanagement/server/ServerApplicationTests.java
+++ server/src/test/java/com/cloudmanagement/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cloudmanagment.server;
+package com.cloudmanagement.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
