6a8865a6c5dfab90e48163946e1b9835269a78d4
Phat <phat.chauu26@gmail.com>
2025-10-02 18:19:23 -0700

    API test passed from URL - end point configured

---
+++ server/src/main/java/com/cloudmanagement/server/TestController.java
@@ -0,0 +1,13 @@
+package com.cloudmanagement.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String testConnection() {
+        return "Server is running and operational!";
+    }
+}
