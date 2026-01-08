f76d9a6ace48288b85f97663b9db8470a29d535b
Phat <phat.chauu26@gmail.com>
2026-01-07 16:45:56 -0800

    committed entry error fixed

--- client/src/components/SpendingChart.tsx
+++ client/src/components/SpendingChart.tsx
@@ -56,7 +56,7 @@ export const SpendingChart: React.FC<SpendingChartProps> = ({ transactions }) =>
                         fill="#8884d8"
                         dataKey="value"
                     >
-                        {data.map((entry, index) => (
+                        {data.map((_, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                         ))}
                     </Pie>
